6aba251cc96c9ac10f978388dbb1b85ee31d871c
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2025-10-21 17:21:40 +0300

    feat: init project

---
+++ src/app/main.tsx
@@ -0,0 +1,13 @@
+import "./globals.css";
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./app";
+
+const root = document.getElementById("root") as HTMLElement;
+
+createRoot(root).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
